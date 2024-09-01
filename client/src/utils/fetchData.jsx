8828db22dc8a3b769da3c97d1ca00f1d4c13d78a
Elvira Gizatullina <elvirawed@gmail.com>
2024-09-02 02:20:06 +0300

    Search

---
+++ client/src/utils/fetchData.jsx
@@ -0,0 +1,16 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "680cbfc5f0msh1b352570382f7a4p13933bjsn1553314ae573",
+    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+// how to change to x-rapidapi-key: import.meta.env.VITE_API_BASE_URL
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
+};
