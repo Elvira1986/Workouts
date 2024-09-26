f2e36a46b900a25d08a16979ff04af690f3a6f69
Elvira Gizatullina <elvirawed@gmail.com>
2024-09-27 00:32:28 +0300

    BodyPart scrollbar added

--- client/src/components/BodyPart.jsx
+++ client/src/components/BodyPart.jsx
@@ -1,46 +1,41 @@
 import React from "react";
 import { Stack, Typography } from "@mui/material";
-import Icon from "../assets/images/Logo.png";
+import Icon from "../assets/icons/gym.png";
 
 const BodyPart = ({ item, bodyPart, setBodyPart }) => {
-  <Stack
-    type="button"
-    alignItems="center"
-    justifyContent="center"
-    className="bodyPart-card"
-    sx={
-      bodyPart === item
-        ? {
-            borderTop: "4px solid #FF2625",
-            background: "#fff",
-            borderBottomLeftRadius: "20px",
-            width: "270px",
-            height: "282px",
-            cursor: "pointer",
-            gap: "47px",
-          }
-        : {
-            background: "#fff",
-            borderBottomLeftRadius: "20px",
-            width: "270px",
-            height: "282px",
-            cursor: "pointer",
-            gap: "47px",
-          }
-    }
-  >
-    <img src={Icon} alt="hello" style={{ width: "40px", height: "40px" }} />
-
-    <Typography
-      fontSize="24px"
-      fontWeight="bold"
-      fontFamily="Alegreya"
-      color="#3A1212"
-      textTransform="capitalize"
+  return (
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={{
+        borderTop: bodyPart === item ? "4px solid #FF2625" : "",
+        background: "#fff",
+        borderBottomLeftRadius: "20px",
+        width: "270px",
+        height: "282px",
+        cursor: "pointer",
+        gap: "47px",
+      }}
+      onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+      }}
     >
-      <p> {item}</p>
-    </Typography>
-  </Stack>;
+      <img src={Icon} alt="" style={{ width: "40px", height: "40px" }} />
+
+      <Typography
+        fontSize="24px"
+        fontWeight="bold"
+        fontFamily="Alegreya"
+        color="#3A1212"
+        textTransform="capitalize"
+      >
+        {item}
+      </Typography>
+    </Stack>
+  );
 };
 
 export default BodyPart;
